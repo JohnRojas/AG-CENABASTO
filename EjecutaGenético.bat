0c0455aaaa9a94f66501fbfe7001e001108e3ea6
JohnRojas <ids_jonathan_rojas@hotmail.com>
2017-06-05 15:47:22 -0500

    Ruta dinámica
    Se realizó la ruta dinámica mediante comandos de Windows.

--- EjecutaGenético.bat
+++ EjecutaGenético.bat
@@ -1,3 +1,3 @@
-java -Xmx6000m -jar "C:\Users\IDS_J\Desktop\Repositorios\AG-CENABASTO\Genetico\Operadores\dist\Operadores.jar"
+java -Xmx6000m -jar "%CD%\Genetico\Operadores\dist\Operadores.jar"
 
 PAUSE
